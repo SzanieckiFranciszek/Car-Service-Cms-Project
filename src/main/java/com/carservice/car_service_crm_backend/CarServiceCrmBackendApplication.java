f6fb0cab9961388d933fb99bd12154b04b2f62a6
Franciszek <franekszaniecki@gmail.com>
2024-11-04 16:53:54 +0100

    Initial commit

---
+++ src/main/java/com/carservice/car_service_crm_backend/CarServiceCrmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.carservice.car_service_crm_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarServiceCrmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarServiceCrmBackendApplication.class, args);
+	}
+
+}
